56469e0947e759bdfcc30bb9960c34b765933c86
Andrew Smith <andrew.smith@rangle.io>
2020-04-26 16:17:02 -0400

    Setup header and footer components

---
+++ src/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>This is the application footer.</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <nav>
+      <a href="#">App 1</a>
+      <a href="#">App 2</a>
+    </nav>
+  );
+}
+
+export default Header;
